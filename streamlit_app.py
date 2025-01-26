71df39f5c5b05e87cabed5ec4668354ab0578b77
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-26 23:01:47 +0000

    Working w/ page management

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
 
+# Initialize session state for page tracking
+if "current_page" not in st.session_state:
+    st.session_state.current_page = "home"
+
+# Define a function for navigation
+def navigate_to(page):
+    st.session_state.current_page = page
+
 # Define pages
 def home_page():
     st.title("ඞ Hello World ඞ")
@@ -33,3 +41,23 @@ def home_page():
         st.success("I warned you...")
     else:
         st.info("WHATEVER YOU DO, DO NOT CLICK THIS BUTTON!")
+
+def page1():
+    st.title("Page 1")
+    st.write("Welcome to Page 1!")
+    if st.button("Go Home"):
+        navigate_to("home")
+
+def page2():
+    st.title("Page 2")
+    st.write("Welcome to Page 2!")
+    if st.button("Go Home"):
+        navigate_to("home")
+
+# Render the current page
+if st.session_state.current_page == "home":
+    home_page()
+elif st.session_state.current_page == "page1":
+    page1()
+elif st.session_state.current_page == "page2":
+    page2()
