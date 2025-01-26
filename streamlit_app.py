f39a755546b5b7f07a0314470ffef4474e7c616e
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-26 22:20:39 +0000

    Add a button

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ st.title("ඞ Hello World ඞ")
 st.write("Why are you here???")
 st.image("res/Dog_side_eye.jpg")
 st.write("Kinda sus")
+st.write(" ")
+
+# Create a button
+if st.button("Click Me"):
+    st.success("Button clicked!")
+else:
+    st.info("Button not clicked yet.")
