bbd2c5d43e4730763360d4586b490ce6499c2b32
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-26 22:25:22 +0000

    Changes to button text

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,15 @@ st.write(" ")
 
 # Create a button
 if st.button("Click Me!"):
-    st.success("Button clicked!")
+    st.success("You clicked me!")
 else:
-    st.info("Button not clicked yet.")
+    st.info("You know you want to...")
 
 # Blank space
 st.write(" ")
 
 # Create a button 2
 if st.button("No, Click Me!"):
-    st.success("Button clicked!")
+    st.success("You clicked me!")
 else:
-    st.info("Button not clicked yet.")
+    st.info("Just do it...")
