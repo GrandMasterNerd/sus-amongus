4996cd6110409dff0309667d798bd30c5799bd5b
GrandMasterNerd <165722873+GrandMasterNerd@users.noreply.github.com>
2025-01-26 22:24:34 +0000

    Added blank spaces

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,18 @@ st.title("ඞ Hello World ඞ")
 st.write("Why are you here???")
 st.image("res/Dog_side_eye.jpg")
 st.write("Kinda sus...")
-#st.write(" ")
+
+# Blank space
+st.write(" ")
 
 # Create a button
 if st.button("Click Me!"):
     st.success("Button clicked!")
 else:
     st.info("Button not clicked yet.")
+
+# Blank space
+st.write(" ")
 
 # Create a button 2
 if st.button("No, Click Me!"):
